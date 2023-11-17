4c2d7cb669128d0a3690592b19e0dec3e17dfddd
Pez Cuckow <email@pezcuckow.com>
2023-11-17 13:13:54 +0100

    Clarify vite config

--- vite.config.js
+++ vite.config.js
@@ -4,18 +4,26 @@ import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
+// Set to this to the name of this collection of components
+// This must match node-red-dashboard-2.widgets[libraryName] in package.json
+const libraryName = 'ui-example'
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue(), cssInjectedByJsPlugin()],
     build: {
-        // Don't minify the library
-        minify: false,
+        // Skip minification in dev mode
+        minify: process.env.NODE_ENV !== 'development',
+
+        // Configure build as a UMD library
         lib: {
             entry: resolve(__dirname, 'ui/index.js'),
-            name: 'ui-example',
-            formats: ['umd', 'es'],
-            fileName: (format, entryName) => `node-red-dashboard.${format}.js`
+            name: libraryName,
+            formats: ['umd'],
+            fileName: (format, entryName) => `${libraryName}.${format}.js`
         },
+
+        // Declare dependencies that shouldn't be bundled into the library
         rollupOptions: {
             // Don't rollup the Vue dependency into the build
             external: ['vue'],
