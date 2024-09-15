4a84c9b747ec2375fe29963160a5a1b591b925a2
ralphwetzel <theonionbox@gmx.com>
2024-09-15 18:05:01 +0200

    Build widget to ui/dist, then copy widget to /resources

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,9 @@
 import { resolve } from 'path'
 
 import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
+import { defineConfig, normalizePath } from 'vite'
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // Set to this to the name of this collection of components
 // This must match node-red-dashboard-2.widgets[libraryName] in package.json
@@ -10,7 +11,19 @@ const LIBRARY_NAME = 'ui-example'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), cssInjectedByJsPlugin()],
+    plugins: [
+        vue(),
+        cssInjectedByJsPlugin(),
+        viteStaticCopy({
+            targets: [
+                {
+                    // Copy the build output into Node-RED's /resources folder
+                    src: normalizePath(resolve(__dirname,`./ui/dist/${LIBRARY_NAME}.umd.js`)),
+                    dest: normalizePath(resolve(__dirname, 'resources'))
+                }
+            ]
+        })
+    ],
     build: {
         // Generate a source map in dev mode
         sourcemap: process.env.NODE_ENV === 'development',
@@ -23,8 +36,8 @@ export default defineConfig({
             fileName: (format, entryName) => `${LIBRARY_NAME}.${format}.js`
         },
 
-        // Utilise Node-RED's handling of /resources folder for the build output
-        outDir: './resources',
+        // This is the target location for the build output
+        outDir: './ui/dist',
 
         // Declare dependencies that shouldn't be bundled into the library
         rollupOptions: {
